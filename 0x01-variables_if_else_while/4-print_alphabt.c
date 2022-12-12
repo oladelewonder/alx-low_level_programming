ec0b9939434651dd6377ffd00a8732c0f8ef4a70
oladelewonder <wonderoladele@gmail.com>
2022-12-12 14:50:18 -0800

    print lowercase without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - print the alpha in lowercase followed by a new line except q and e
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		if (lc != 'e' && lc != 'q')
+			putchar(lc);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
