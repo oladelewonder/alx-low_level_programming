8db6697790ecd40644fdcbd3621cef8c6e1b57e7
oladelewonder <wonderoladele@gmail.com>
2022-12-13 13:45:59 -0800

    fix

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+/**
+ * _putchar - write the charater c to stdout
+ * @c: the charater to print
+ *
+ * Return: On success 1
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"0-putchar.c" 11L, 180C                                       10,24-31      All
+
