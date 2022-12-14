e709cacf6a9edf4a25c8a2f66eb7c3712a3b920d
oladelewonder <wonderoladele@gmail.com>
2022-12-13 22:29:47 -0800

    another fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,10 @@ void print_alphabet_x10(void)
 	int ten;
 	int la;
 
-	for (ten = 0; ten <= 9; ten++)
+	for (ten = 0; ten <= 10; ten++)
 	{
 		for (la = 'a'; la <= 'z'; la++)
 			_putchar(la);
+
 		_putchar('\n');
 	}
