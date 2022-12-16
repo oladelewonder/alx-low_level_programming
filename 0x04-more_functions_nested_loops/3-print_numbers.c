27ca86bd4d604c5c25ba17e8c7d94a5effc1d92c
oladelewonder <wonderoladele@gmail.com>
2022-12-16 01:24:11 -0800

    print all numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * print_numbers - function that print all numbers from 0 to 9
+ *
+ * Return: return void
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchat(n);
+	}
+	putchar(10);
+}
