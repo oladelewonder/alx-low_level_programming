e21b1c034212dbaaf07c764cedcfa220cd3bf78b
oladelewonder <wonderoladele@gmail.com>
2023-01-03 03:39:53 -0800

    fix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * file: main.h
- * description: header file containing prototypes for all functions
- * used in then 0x07-pointers_arrays_strings directory.
- */
-#include <stdout.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
